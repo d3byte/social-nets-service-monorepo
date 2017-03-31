74926430032bcb43406bc0ea89069282abe807e1
Dmitry Nikulin <russiankenny@gmail.com>
2017-03-31 13:58:13 +0300

    JS Template is fully completed. Need to fill it up.

--- js/buttons.js
+++ js/buttons.js
@@ -7,4 +7,28 @@ $(document).ready(function(){
     $(".yt").removeClass("show").addClass("hide");
     $("#youtube_services_more_subs").removeClass("hide").addClass("show");
   })
+  $("#btn3").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_order").removeClass("hide").addClass("show");
+  })
+  $("#btn4").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_dis").removeClass("hide").addClass("show");
+  })
+  $("#btn5").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_fav").removeClass("hide").addClass("show");
+  })
+  $("#btn6").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_com").removeClass("hide").addClass("show");
+  })
+  $("#btn7").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_like").removeClass("hide").addClass("show");
+  })
+  $("#btn8").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_rep").removeClass("hide").addClass("show");
+  })
 });
