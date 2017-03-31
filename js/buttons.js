94bf2b5c478ba4a43b4403c72b1e9204a0faa24f
Dmitry Nikulin <russiankenny@gmail.com>
2017-03-31 13:53:18 +0300

    jquery additions

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $("#btn1").on("click", function(){
-    $("#youtube_services_more_views").removeClass("hide").addClass("show");
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_views").removeClass("hide").addClass("show").fadein();
+  });
+  $("#btn2").on("click", function(){
+    $(".yt").removeClass("show").addClass("hide");
+    $("#youtube_services_more_subs").removeClass("hide").addClass("show");
   })
 });
