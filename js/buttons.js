7f160ecf81b8e5f3edad259b667aef3e4c33b3ed
DieRK <russiankenny@gmail.com>
2017-04-11 20:48:45 +0300

    Fixed the JS, removed twitter

--- js/buttons.js
+++ js/buttons.js
@@ -4,46 +4,50 @@ $(document).ready(function(){
     $(".inst").hide();
     $(".twt").hide();
     $("#youtube_services").show();
+    $("#order").hide();
+    $("#chs_cat").show()
   })
   $("#btn1").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
     $("#youtube_views").show();
+    $("#order").show();
   })
   $("#btn2").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
     $("#youtube_subs").show();
+    $("#order").show();
   })
   $("#btn3").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
-    $("#youtube_order").show();
+    $("#youtube_dis").show();
+    $("#order").show();
   })
   $("#btn4").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
-    $("#youtube_dis").show();
+    $("#youtube_fav").show();
+    $("#order").show();
   })
   $("#btn5").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
-    $("#youtube_fav").show();
+    $("#youtube_com").show();
+    $("#order").show();
   })
   $("#btn6").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
-    $("#youtube_com").show();
+    $("#youtube_like").show();
+    $("#order").show();
   })
   $("#btn7").on("click", function(){
     $("#chs_yt").show();
     $(".yt_serv").hide();
-    $("#youtube_like").show();
-  })
-  $("#btn8").on("click", function(){
-    $("#chs_yt").show();
-    $(".yt_serv").hide();
     $("#youtube_rep").show();
+    $("#order").show();
   })
 
 
@@ -52,58 +56,38 @@ $(document).ready(function(){
     $(".yt").hide();
     $("#instagram_services").show();
     $(".twt").hide();
+    $("#order").hide();
+    $("#chs_cat").show()
+  })
+  $("#btn8").on("click", function(){
+    $("#chs_inst").show();
+    $(".inst_serv").hide();
+    $("#instagram_subs").show();
+    $("#order").show();
   })
   $("#btn9").on("click", function(){
     $("#chs_inst").show();
     $(".inst_serv").hide();
-    $("#instagram_subs").show();
+    $("#instagram_likes").show();
+    $("#order").show();
   })
   $("#btn10").on("click", function(){
     $("#chs_inst").show();
     $(".inst_serv").hide();
-    $("#instagram_likes").show();
+    $("#instagram_autolikes").show();
+    $("#order").show();
   })
   $("#btn11").on("click", function(){
     $("#chs_inst").show();
     $(".inst_serv").hide();
-    $("#instagram_autolikes").show();
+    $("#instagram_views").show();
+    $("#order").show();
   })
   $("#btn12").on("click", function(){
     $("#chs_inst").show();
     $(".inst_serv").hide();
-    $("#instagram_views").show();
-  })
-  $("#btn13").on("click", function(){
-    $("#chs_inst").show();
-    $(".inst_serv").hide();
-    $("#instagram_complex").show();
-  })
-  $("#btn14").on("click", function(){
-    $("#chs_inst").show();
-    $(".inst_serv").hide();
     $("#instagram_comments").show();
+    $("#order").show();
   })
 
-
-  // твиттор
-  $("#btn_twt").on("click",function(){
-    $(".yt").hide();
-    $(".inst").hide();
-    $("#twt_services").show();
-    })
-    $("#btn21").on("click", function(){
-      $("#chs_twt").show();
-      $(".twt_serv").hide();
-      $("#twitter_subs").show();
-    })
-    $("#btn22").on("click", function(){
-      $("#chs_twt").show();
-      $(".twt_serv").hide();
-      $("#twitter_retweets").show();
-    })
-    $("#btn23").on("click", function(){
-      $("#chs_twt").show();
-      $(".twt_serv").hide();
-      $("#twitter_favourites").show();
-    })
 });
