c26e1a7cfdaf3f55edf62df67c742c7b7fb2ce82
Dmitry Nikulin <russiankenny@gmail.com>
2017-04-15 20:09:38 +0300

    landing edits

--- php/landing.php
+++ php/landing.php
@@ -79,11 +79,6 @@
   <div id="date"></div>
   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
