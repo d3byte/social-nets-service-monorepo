b9741f027a58554728d30079277dc0cd51cf2a4e
Pandinator <cheloveckov.ru@yandex.ru>
2017-04-16 23:40:50 +0300

    Zzhoopa commit

--- php/landing.php
+++ php/landing.php
@@ -1,6 +1,5 @@
 <?php
   require 'libs/config.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -27,147 +26,132 @@
 <body>
 
   <nav class="navbar navbar-default navbar-inverse">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+      <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
         <a class="navbar-brand" href="#"><?php echo $shopName; ?></a>
       </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-        <ul class="nav navbar-nav navbar-right">
+      <ul class="nav navbar-nav navbar-right">
           <li class="active"><a href="#about">Немного о нас</a></li>
-          <li><a href="../php/register.php">Регистрация</a></li>
+          <li><a href="../php/signup.php">Регистрация</a></li>
           <li><a href="../php/signin.php">Вход</a></li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
-
+      </ul>
+  </div>
+  </div>
+</nav>
 <div class="container" id="about">
   <center>
-<h1> Лучшим людям предлагаем лучших подписчиков! <br>
-  Предлагаем пролистать чуть ниже, <br>
-   чтобы познакомиться с нами поближе.
- </h1>
- <br>
-<i class="icono-caretDown"></i>
-<h2> Что мы можем тебе предложить?</h2>
-<div class="row">
-  <center>
-  <div class="col-lg-4 left-bar">
-    <h3>Живое продвижение</h3>
-    <p> Недорого, быстро, удобно </p>
-  </div>
-  <div class="col-lg-4"></div>
-  <div class="col-lg-4 right-bar">
-    <h3>Накрутка мёртвых душ </h3>
-    <p> Ещё быстрее, ещё дешевле, ещё удобнее </p>
-  </div>
-  </center>
+      <h1> Лучшим людям предлагаем лучших подписчиков! <br>
+Предлагаем пролистать чуть ниже, <br>
+чтобы познакомиться с нами поближе.
+</h1>
+      <br>
+      <i class="icono-caretDown"></i>
+      <h2> Что мы можем тебе предложить?</h2>
+      <div class="row">
+          <center>
+              <div class="col-lg-4 left-bar">
+                  <h3>Живое продвижение</h3>
+                  <p> Недорого, быстро, удобно </p>
+              </div>
+              <div class="col-lg-4"></div>
+              <div class="col-lg-4 right-bar">
+                  <h3>Накрутка мёртвых душ </h3>
+                  <p> Ещё быстрее, ещё дешевле, ещё удобнее </p>
+              </div>
+          </center>
+      </div>
 </div>
-</div>
-
 <div class="container" id="results">
   <center>
-<h1> Результаты на сегодня </h1>
-<div id="date"></div>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="../src/scr1.jpg" alt="...">
-      <div class="carousel-caption">
+      <h1> Результаты на сегодня </h1>
+      <div id="date"></div>
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                  <img src="../src/scr1.jpg" alt="...">
+                  <div class="carousel-caption">
+                  </div>
+              </div>
+              <div class="item">
+                  <img src="../src/scr2.jpg" alt="...">
+                  <div class="carousel-caption">
+                  </div>
+              </div>
+              <div class="item">
+                  <img src="../src/scr3.jpg" alt="...">
+                  <div class="carousel-caption">
+                  </div>
+              </div>
+              <div class="item">
+                  <img src="../src/scr4.jpg" alt="...">
+                  <div class="carousel-caption">
+                  </div>
+              </div>
+          </div>
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+          </a>
       </div>
-    </div>
-    <div class="item">
-      <img src="../src/scr2.jpg" alt="...">
-      <div class="carousel-caption">
-      </div>
-    </div>
-    <div class="item">
-      <img src="../src/scr3.jpg" alt="...">
-      <div class="carousel-caption">
-      </div>
-    </div>
-    <div class="item">
-      <img src="../src/scr4.jpg" alt="...">
-      <div class="carousel-caption">
-      </div>
-    </div>
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  </center>
 </div>
-</center>
-</div>
-
 <div class="container" id="numbers">
-<center>
-  <h1> Мы в цифрах </h1>
-  <br>
-  <div class="row">
-    <div class="col-lg-3">
-      <i class="icono-check"></i>
-      <h3> 2724 </h3>
-      <p class="semi-tr"> Успешных заказов </p>
-    </div>
-    <div class="col-lg-3">
-      <i class="icono-user"></i>
-      <h3> 34М </h3>
-      <p class="semi-tr"> Подписчиков добавили </p>
-    </div>
-    <div class="col-lg-3">
-      <i class="icono-sun"></i>
-      <h3> 8М </h3>
-      <p class="semi-tr"> Лайков </p>
-    </div>
-    <div class="col-lg-3">
-      <i class="icono-clock"></i>
-      <h3> 24 часа </h3>
-      <p class="semi-tr"> Часы работы службы поддержки </p>
-    </div>
-  </div>
-</center>
+  <center>
+      <h1> Мы в цифрах </h1>
+      <br>
+      <div class="row">
+          <div class="col-lg-3">
+              <i class="icono-check"></i>
+              <h3> 2724 </h3>
+              <p class="semi-tr"> Успешных заказов </p>
+          </div>
+          <div class="col-lg-3">
+              <i class="icono-user"></i>
+              <h3> 34М </h3>
+              <p class="semi-tr"> Подписчиков добавили </p>
+          </div>
+          <div class="col-lg-3">
+              <i class="icono-sun"></i>
+              <h3> 8М </h3>
+              <p class="semi-tr"> Лайков </p>
+          </div>
+          <div class="col-lg-3">
+              <i class="icono-clock"></i>
+              <h3> 24 часа </h3>
+              <p class="semi-tr"> Часы работы службы поддержки </p>
+          </div>
+      </div>
+  </center>
 </div>
-
 <div class="container" id="register">
   <center>
-    <h1> Зарегистрируйся прямо сейчас <br> и закажи своих первых подписчиков уже <br>
-      через 7 секунд после регистрации! </h1>
+      <h1> Зарегистрируйся прямо сейчас <br> и закажи своих первых подписчиков уже <br>
+через 7 секунд после регистрации! </h1>
       <button name="submit" type="submit" class="btn btn-success btn-outline" href="../php/register.php">Зарегистрироваться</button>
   </center>
 </div>
-
 <footer>
-
-    <div class="footer" id="footer">
-        <div class="container">
-            <div class="row">
-                    <center>
-                        <a href="//www.free-kassa.ru/"><img src="//www.free-kassa.ru/img/fk_btn/22.png"></a>
-                        <br>
-                        <p style="color:gray">Made with love by <span><a id="landhref" href="http://izerrio.pro">iZerrio Studio, 2017 </a></span></p>
-                    </center>
-            </div>
-            <!--/.row-->
-        </div>
-        <!--/.container-->
-    </div>
+  <div class="footer" id="footer">
+      <div class="container">
+          <div class="row">
+              <center>
+                  <a href="//www.free-kassa.ru/"><img src="//www.free-kassa.ru/img/fk_btn/22.png"></a>
+                  <br>
+                  <p style="color:gray">Made with love by <span><a id="landhref" href="http://izerrio.pro">iZerrio Studio, 2017 </a></span></p>
+              </center>
+          </div>
+      </div>
+  </div>
+</footer>
 </body>
-
 </html>
--- php/libs/config.php
+++ php/libs/config.php
@@ -1,11 +1,11 @@
 <?php
   // Здесь вы можете изменить данные для подключения к базе данных
-  $dbHost = "localhost"; // Адрес базы данных
-  $dbName = "nickro_dierk"; // Имя базы данных
-  $dbLogin = "nickro_dierk"; // Логин PHPMyAdmin
-  $dbPassword = "NE302XDESk6zOPP"; // Пароль PHPMyAdmin
+  $dbHost = "127.0.0.1"; // Адрес базы данных
+  $dbName = "zerrio_nakrutka"; // Имя базы данных
+  $dbLogin = "zerrio_nakrutka"; // Логин PHPMyAdmin
+  $dbPassword = "root123"; // Пароль PHPMyAdmin
   // Основные настройки
-  $shopName = '*djuuuu'; // Название магазина
+  $shopName = '*djumandji*'; // Название магазина
   // Free-kassa
   $merchant_id = '48966'; // id магазина
   $secret_word = '1fthtsfk'; // Секретное слово
